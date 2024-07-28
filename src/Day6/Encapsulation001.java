edf687d2dc7dbbe057ce030641833099891ee20a
Raghav <Raghav@192.168.0.197>
2024-07-29 00:27:05 +0530

    adding additional data

--- src/Day6/Encapsulation001.java
+++ src/Day6/Encapsulation001.java
@@ -6,8 +6,8 @@ public class Encapsulation001 {
 	 * 
 	 * 
 	 * 1. All variables should be private
-	 * for every variables  there should be 2 methods (get and set)
-	 * variables can be operated only through methods
+	 * 2.for every variables  there should be 2 methods (get and set)
+	 * 3.variables can be operated only through methods
 	 */
 
 }
