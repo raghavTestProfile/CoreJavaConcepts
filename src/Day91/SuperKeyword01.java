1e3dd12ccd7788bf946b80069cc7ee2e96b8edcc
Raghav <Raghav@192.168.0.197>
2024-08-01 14:56:46 +0530

    Commit - 1/8.  Adding more Core Java Concepts

---
+++ src/Day91/SuperKeyword01.java
@@ -0,0 +1,35 @@
+package Day91;
+
+public class SuperKeyword01 {
+
+	String color = "white";
+
+	void eat() {
+
+		System.out.println("eating parent class method");
+	}
+}
+
+class Extends extends SuperKeyword01 {
+
+	String color = "black";
+
+	void displayColor() {
+
+		System.out.println(color);
+
+	}
+
+	void displayColor2() {
+
+		System.out.println(super.color); // when super is used it will call value from the parent class
+
+	}
+
+	void eat() {
+
+		// System.out.println("eating child class method");
+		super.eat();
+	}
+
+}
---
+++ src/Day91/SuperKeyword02.java
@@ -0,0 +1,15 @@
+package Day91;
+
+public class SuperKeyword02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Extends obj1 = new Extends();
+		obj1.displayColor();
+		obj1.displayColor2();
+		obj1.eat();
+
+	}
+
+}
